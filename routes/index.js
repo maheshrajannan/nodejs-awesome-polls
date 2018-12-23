460ef396e3e458962c48687365df876e6bf7fbeb
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2018-12-22 18:18:52 -0600

    display email
    display email and redirect to error

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var passport = require('passport');
 var router = express.Router();
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var request = require('request');
+var util = require('util');
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -31,9 +32,17 @@ router.get('/polls', ensureLoggedIn, function(req, res){
   request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president', function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var polls = JSON.parse(body);
+      console.log("request:"+util.inspect(req));
       console.log("user:"+JSON.stringify(req.user));
-      console.log("user emails:"+JSON.stringify(req.user.emails[0]));
-      console.log("user emails:"+req.user.emails[0]);
+      console.log("user emails stringify:"+JSON.stringify(req.user.emails[0]));
+      console.log("user emails object:"+req.user.emails[0]);
+      console.log("user emails value:"+JSON.stringify(req.user.emails[0].value));
+      if(req.user.nickname) {
+        console.log("No need to change nickname");
+      } else {
+        req.user.nickname=req.user.emails[0].value;
+        console.log("Nick name changed to "+req.user.nickname);
+      }
       res.render('polls', {user: req.user, polls: polls});
     } else {
       res.render('error');
@@ -42,12 +51,12 @@ router.get('/polls', ensureLoggedIn, function(req, res){
 })
 
 router.get('/user', ensureLoggedIn, function(req, res, next) {
-  req.user.email='mahesh.rajannan@gmail.com';
+  //req.user.email='mahesh.rajannan@gmail.com';
   res.render('user', { user: req.user });
 });
 
 router.get('/callback',
-  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  passport.authenticate('auth0', { failureRedirect: '/error' }),
   function(req, res) {
     res.redirect(req.session.returnTo || '/polls');
   });
